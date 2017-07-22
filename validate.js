a5a094f243c7049486c3fc66f5c7521dcef67898
robin <robin@makichan.moe>
2017-07-22 18:32:58 +0900

    간단한 검사기 제작

---
+++ validate.js
@@ -0,0 +1,17 @@
+const validator = require('validator');
+
+module.exports = (value, rule) => {
+	switch(rule.type) {
+		case 'number':
+			return /^\d+$/.test(value);
+		case 'email':
+			return validator.isEmail(value);
+		case 'string':
+			return validator.isLength(value, {min: rule.min, max: rule.max});
+		case 'uuid':
+			return validator.isUUID(val);
+		default:
+			console.warn(`Unknown type ${rule.type}`);
+			return true;
+	}
+};
