1f316c16dd7ae9210453b1571c48a420cdf2cff8
robin <robin@makichan.moe>
2017-07-22 18:16:38 +0900

    결과값용 클래스 제작

---
+++ MaruResult.js
@@ -0,0 +1,20 @@
+class MaruResult {
+	/**
+	 * Create a MaruResult object.
+	 * @param {Number} [code=200] - HTTP status code to return.
+	 * @param {Object} [content] - An object to return.
+	 */
+	constructor(code, content) {
+		if(content === undefined) {
+			content = code;
+			code = 200;
+		}
+		this.content = content;
+		this.code = code;
+	}
+	cookResult(res) {
+		res.status(this.code).json(this.content || {});
+	}
+}
+
+module.exports = MaruResult;
