d42c54d51a621473da394b17a66d5c68004169f7
robin <robin@makichan.moe>
2017-07-27 00:52:38 +0900

    설정 사이트 틀 제작

---
+++ setting/public/js/main.js
@@ -0,0 +1,29 @@
+let app = new Vue({
+	el: '#main',
+	data: {
+		setting: {}, newRequest: ''
+	},
+	methods: {
+		styleBadge(method) {
+			switch(method) {
+				case 'get':
+					return 'label-primary';
+				case 'post':
+					return 'label-success';
+				case 'put':
+					return 'label-info';
+				case 'delete':
+					return 'label-warning';
+			}
+		},
+		addRequests(prop, name) {
+			if(name) {
+				this.$set(prop, name, {type: 'string', source: 'body', name, optional: false});
+			}
+		}
+	}
+});
+
+$.getJSON('/update', (data) => {
+	app.setting = data;
+});
