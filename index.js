3bbab87977222d42a36fc8b076ea06b5360ced93
robin <robin@makichan.moe>
2017-07-26 23:54:37 +0900

    새로운 형식에 맞춰서 변경

--- index.js
+++ index.js
@@ -17,81 +17,84 @@ module.exports = (settings) => {
 		start(port) {
 			for(let f in settings.path) {
 				if(settings.path.hasOwnProperty(f)) {
-					const $this = settings.path[f];
-					let requestObject = {};
-					let worker = (req, res) => {
-						if($this.hasOwnProperty('requests')) {
-							for(let n in $this.requests) {
-								if($this.requests.hasOwnProperty(n)) {
-									let value;
-									const sourceName = $this.requests[n].name || n;
-									switch($this.requests[n].source) {
-										case 'query':
-											value = req.query[sourceName];
-											break;
-										case 'url':
-											value = req.params[sourceName];
-											break;
-										case 'header':
-											value = req.get(sourceName);
-											break;
-										case 'body':
-										default:
-											value = req.body[sourceName];
-											break;
-									}
-									if(!value) {
-										if(!$this.requests[n].optional) {
-											new MaruResult(400, {error: 'Missing variable', variableName: sourceName}).cookResult(res);
-											return;
-										}
-										else {
-											continue;
-										}
-									}
-									if(validate(value, $this.requests[n])) {
-										requestObject[n] = $this.requests[n].type !== 'number' ? value : Number(value);
-									}
-									else {
-										if(!$this.requests[n].optional) {
-											new MaruResult(400, {error: 'Invalid variable', variableName: sourceName}).cookResult(res);
-											return;
+					for(let m in settings.path[f]) {
+						if(settings.path[f].hasOwnProperty(m)) {
+							const $this = settings.path[f][m];
+							let requestObject = {};
+							let worker = (req, res) => {
+								if($this.hasOwnProperty('requests')) {
+									for(let n in $this.requests) {
+										if($this.requests.hasOwnProperty(n)) {
+											let value;
+											const sourceName = $this.requests[n].name || n;
+											switch($this.requests[n].source) {
+												case 'query':
+													value = req.query[sourceName];
+													break;
+												case 'url':
+													value = req.params[sourceName];
+													break;
+												case 'header':
+													value = req.get(sourceName);
+													break;
+												case 'body':
+												default:
+													value = req.body[sourceName];
+													break;
+											}
+											if(!value) {
+												if(!$this.requests[n].optional) {
+													new MaruResult(400, {error: 'Missing variable', variableName: sourceName}).cookResult(res);
+													return;
+												}
+												else {
+													continue;
+												}
+											}
+											if(validate(value, $this.requests[n])) {
+												requestObject[n] = $this.requests[n].type !== 'number' ? value : Number(value);
+											}
+											else {
+												if(!$this.requests[n].optional) {
+													new MaruResult(400, {error: 'Invalid variable', variableName: sourceName}).cookResult(res);
+													return;
+												}
+											}
 										}
 									}
 								}
+								if(functions.hasOwnProperty($this.func)) {
+									functions[$this.func].call(null, requestObject).then((result) => {
+										if(!(result instanceof MaruResult)) {
+											result = new MaruResult(result);
+										}
+										result.cookResult(res);
+									}, (result) => {
+										if(!(result instanceof MaruResult)) {
+											result = new MaruResult(500, result);
+										}
+										result.cookResult(res);
+									});
+								}
+								else {
+									new MaruResult(501, {error: 'Invalid server config'}).cookResult(res);
+								}
+							};
+							switch(m) {
+								case 'post':
+									app.post(f, worker);
+									break;
+								case 'put':
+									app.put(f, worker);
+									break;
+								case 'delete':
+									app.delete(f, worker);
+									break;
+								case 'get':
+									app.get(f, worker);
+									break;
 							}
 						}
-						if(functions.hasOwnProperty($this.func)) {
-							functions[$this.func].call(null, requestObject).then((result) => {
-								if(!(result instanceof MaruResult)) {
-									result = new MaruResult(result);
-								}
-								result.cookResult(res);
-							}, (result) => {
-								if(!(result instanceof MaruResult)) {
-									result = new MaruResult(500, result);
-								}
-								result.cookResult(res);
-							});
-						}
-						else {
-							new MaruResult(501, {error: 'Invalid server config'}).cookResult(res);
-						}
-					};
-					switch($this.method) {
-						case 'post':
-							app.post(f, worker);
-							break;
-						case 'put':
-							app.put(f, worker);
-							break;
-						case 'delete':
-							app.delete(f, worker);
-							break;
-						case 'get':
-						default:
-							app.get(f, worker);
-							break;
 					}
 				}
 			}
